a2d22d7439e359f2124ef488bae321ad7a1c5c92
Juan Diaz <jfdiaza@unal.edu.co>
2022-12-06 08:16:26 -0500

    create like model file

---
+++ app/models/like.rb
@@ -0,0 +1,11 @@
+class Like < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post, class_name: 'Post'
+    after_save :increase_likes_counter
+
+    private
+    
+    def increase_likes_counter
+        post.increment!(:likes_counter)
+    end
+end
