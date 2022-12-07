5b124ca43708a6b5339aff9171cdc082a58cdda4
Juan Diaz <jfdiaza@unal.edu.co>
2022-12-07 06:47:42 -0500

    finish validations and create rspec test files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
-  validates :post_counter, presence: true,
+  validates :posts_counter, presence: true,
                            numericality: {
                             only_integer: true, greater_than_or_equal_to: 0
                            }
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe Post, type: :model do
+  
+  before(:each) do
+
+    @test_post = Post.create(title: 'Post1',
+                        text: 'Post text',
+                        author: @test_user)
+    end
+
+  context 'testing validation for Post model' do
+    it 'validate title cant be empty' do
+      @test_post.title = nil
+      expect(@test_post).to_not be_valid
+    end
+  end
+
+  context 'testing validation for Post model' do
+    it 'validate title cant be greater than 250 characters' do
+      @test_post.title = 'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
+      expect(@test_post).to_not be_valid
+    end
+  end
+
+  context 'testing validation for Post model' do
+    it 'validate comments counter cant be empty' do
+      @test_post.comments_counter = nil
+      expect(@test_post).to_not be_valid
+    end
+  end
+
+  context 'testing validation for Post model' do
+    it 'validate comments counter cant be negative' do
+      @test_post.comments_counter = -1
+      expect(@test_post).to_not be_valid
+    end
+  end
+
+  context 'testing validation for Post model' do
+    it 'validate likes counter cant be empty' do
+      @test_post.likes_counter = nil
+      expect(@test_post).to_not be_valid
+    end
+  end
+
+  context 'testing validation for Post model' do
+    it 'validate likes counter cant be negative' do
+      @test_post.likes_counter = -1
+      expect(@test_post).to_not be_valid
+    end
+  end
+
+  context 'testing method for Post model' do
+    it 'should have 0 comments' do
+      expect(@test_post.most_recent_comments.count).to eq(0)
+    end
+  end
+end
